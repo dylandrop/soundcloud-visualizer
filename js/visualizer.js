bf7b78857264e38abd7c39fb89a80cb3d08798ad
Dylan <dylandrop@gmail.com>
2013-10-01 13:03:06 -0400

    basic bouncing

--- js/visualizer.js
+++ js/visualizer.js
@@ -24,17 +24,8 @@ var VisualizerDot = (function() {
 
    function VisualizerDot(x, y, c) {
       this.xPos = x; this.yPos = y; 
-      this.minYPos = 0; this.minXPos = 0;
       this.color = c; this.radius = 5;
-      this.vel = 4;
-   }
-
-   VisualizerDot.prototype.setMinX = function(x) {
-      this.minXPos = x;
-   }
-
-   VisualizerDot.prototype.setMinY = function(y) {
-      this.minYPos = y;
+      this.vel = 3;
    }
 
    VisualizerDot.prototype.setAngle = function(theta) {
@@ -43,19 +34,22 @@ var VisualizerDot = (function() {
 
    VisualizerDot.prototype.updateX = function() {
       this.xPos += this.vel * Math.cos(this.angle);
-      this.vel -= 0.1; 
+      this.vel -= 0.2; //gravity
    }
 
    VisualizerDot.prototype.updateY = function() {
       this.yPos += this.vel * Math.sin(this.angle);
-      this.vel -= 0.1; 
-      // if((this.yPos + this.radius) > this.minYPos) {
-      //    this.vel = -1 * this.vel;
-      // }
+   }
+
+   VisualizerDot.prototype.bounceBack = function(otherObjX, otherObjY, otherObjRad) {
+      if( Math.sqrt(Math.pow(otherObjY - this.yPos, 2) + Math.pow(otherObjX - this.xPos,2)) < (otherObjRad+this.radius)) {
+         this.vel = -1 * this.vel * 0.8;//bouncefactor == 0.9
+      }
    }
 
    VisualizerDot.prototype.draw = function() {
       context.beginPath();
+      this.bounceBack(artist.xPos, artist.yPos, artist.radius);
       this.updateY(); this.updateX();
       context.arc(this.xPos, this.yPos, this.radius, 0, 2 * Math.PI, false);
       context.fillStyle = this.color;
@@ -66,11 +60,10 @@ var VisualizerDot = (function() {
 })();
 
 function initializeDotsAboutRadius(radius, offset, dots) {
-   for(var i = 1; i <= 45; i++) {
-      angle = 2 * Math.PI * i/45;
+   var numDots = 45;
+   for(var i = 1; i <= numDots; i++) {
+      angle = 2 * Math.PI * i/numDots;
       dots.push(new VisualizerDot(Math.cos(angle) * (radius + offset + 5) + centerX, Math.sin(angle) * (radius + offset + 5) + centerY, "#FFFFFF"));
-      dots[i-1].setMinX(Math.cos(angle) * (radius + 5) + centerX);
-      dots[i-1].setMinY(Math.sin(angle) * (radius + 5) + centerX);
       dots[i-1].setAngle(angle);
       dots[i-1].draw();
    }
@@ -97,7 +90,7 @@ var artist = new CenterIcon(centerX, centerY, radius);
 artist.draw(context);
 
 var dots = new Array();
-initializeDotsAboutRadius(radius, 70, dots);
+initializeDotsAboutRadius(radius, 100, dots);
 
 setInterval(draw,1000/60);
 
