1fb7090f94499df64ba92812f09863633e3de3ed
Dylan <dylandrop@gmail.com>
2013-10-01 11:08:07 -0400

    Ok at least gravity is working

--- js/visualizer.js
+++ js/visualizer.js
@@ -1,23 +1,20 @@
 var CenterIcon = (function() {
-   var radius;
-   var xPos;
-   var yPos;
 
    function CenterIcon(x, y, rad) {
-      xPos = x;
-      yPos = y;
-      radius = rad;
+      this.xPos = x;
+      this.yPos = y;
+      this.radius = rad;
    }
 
    CenterIcon.prototype.draw = function() {
       context.beginPath();
-      context.arc(xPos, yPos, radius, 0, 2 * Math.PI, false);
+      context.arc(this.xPos, this.yPos, this.radius, 0, 2 * Math.PI, false);
       context.fillStyle = 'green';
       context.fill();
    }
 
    CenterIcon.prototype.getRadius = function() {
-        return radius;
+        return this.radius;
    };
 
    return CenterIcon;
@@ -27,7 +24,9 @@ var VisualizerDot = (function() {
 
    function VisualizerDot(x, y, c) {
       this.xPos = x; this.yPos = y; 
+      this.minYPos = 0; this.minXPos = 0;
       this.color = c; this.radius = 5;
+      this.vel = 4;
    }
 
    VisualizerDot.prototype.setMinX = function(x) {
@@ -38,17 +37,26 @@ var VisualizerDot = (function() {
       this.minYPos = y;
    }
 
+   VisualizerDot.prototype.setAngle = function(theta) {
+      this.angle = theta;
+   }
+
    VisualizerDot.prototype.updateX = function() {
-      this.xPos = this.xPos + 1;
+      this.xPos += this.vel * Math.cos(this.angle);
+      this.vel -= 0.1; 
    }
 
    VisualizerDot.prototype.updateY = function() {
-      this.yPos = this.yPos + 1;
+      this.yPos += this.vel * Math.sin(this.angle);
+      this.vel -= 0.1; 
+      // if((this.yPos + this.radius) > this.minYPos) {
+      //    this.vel = -1 * this.vel;
+      // }
    }
 
    VisualizerDot.prototype.draw = function() {
       context.beginPath();
-      this.updateX(); this.updateY();
+      this.updateY(); this.updateX();
       context.arc(this.xPos, this.yPos, this.radius, 0, 2 * Math.PI, false);
       context.fillStyle = this.color;
       context.fill();
@@ -63,11 +71,14 @@ function initializeDotsAboutRadius(radius, offset, dots) {
       dots.push(new VisualizerDot(Math.cos(angle) * (radius + offset + 5) + centerX, Math.sin(angle) * (radius + offset + 5) + centerY, "#FFFFFF"));
       dots[i-1].setMinX(Math.cos(angle) * (radius + 5) + centerX);
       dots[i-1].setMinY(Math.sin(angle) * (radius + 5) + centerX);
+      dots[i-1].setAngle(angle);
       dots[i-1].draw();
    }
 }
 
 function draw() {
+   context.clearRect(0,0,canvas.width,canvas.height);
+   artist.draw();
    for(var i = 0; i < dots.length; i++) {
       dots[i].draw();
    }
@@ -88,5 +99,5 @@ artist.draw(context);
 var dots = new Array();
 initializeDotsAboutRadius(radius, 70, dots);
 
-setInterval(draw,1000);
+setInterval(draw,1000/60);
 
